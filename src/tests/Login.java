1c136604a4aee005c6bffe5b08c27327ed85c66a
fkundysk <fkundysk@gmail.com>
2020-05-07 00:35:20 +0200

    updated Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -4,7 +4,7 @@ public class Login {
 
     public static void main(String[] args) {
 
-        System.out.println("Login to Git");
+        System.out.println("Login to Gitgit add .");
         
         //I added this line from GitHub
         // I want to see lines in IntelJ
