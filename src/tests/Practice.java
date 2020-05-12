ee9443664da3fc651dd9706e69302e3e395408dd
fkundysk <61801854+fkundysk@users.noreply.github.com>
2020-05-12 17:05:55 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -5,6 +5,12 @@ public class Practice {
     public static void main(String[] args) {
 
         System.out.println("Practice");
+        
+        // These lines from GitHub
+        // Hello Tuesday
+        // Hi EU-2
+        
+        
 
     }
 }
